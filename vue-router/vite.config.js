d36264e09677d167c96714cb0ef186a138d12117
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-08-17 00:34:19 +0700

    Membuat semua komponent yang di build menjadi single javascript file

--- vue-router/vite.config.js
+++ vue-router/vite.config.js
@@ -4,4 +4,21 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          users: [
+            './src/components/tugas9/User.vue',
+            './src/components/tugas9/UserHeader.vue',
+            './src/components/tugas4/UserProfile.vue',
+            './src/components/tugas4/UserOrder.vue',
+            './src/components/tugas4/UserWishlist.vue',
+            './src/components/tugas9/UserOrderFooter.vue',
+            './src/components/tugas9/UserWishlistFooter.vue', 
+          ]
+        }
+      }
+    }
+  } 
 })
