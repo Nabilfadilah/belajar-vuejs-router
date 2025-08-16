5b1fba95fa8c496158f91eaf525bd5b1fdf7d78d
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-08-16 16:46:19 +0700

    Membuat object router

--- vue-router/src/main.js
+++ vue-router/src/main.js
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router'
 
-createApp(App).mount('#app')
+// object router 
+const router = createRouter({
+    routes: [],
+    history: createWebHistory()
+})
+
+createApp(App).use(router).mount('#app')
